adb7e0b0d05a0d02745953a0c2062fb880c7dfcd
ZanGunawan <zunzee10@gmail.com>
2024-02-12 10:50:23 +0700

    perbaikan desain login dan sign up

--- pages/index.vue
+++ pages/index.vue
@@ -1,71 +1,73 @@
 <template>
   <div class="main">
-    <div class="container d-flex justify-content-center">
-      <div class="">
-        <div class="d-flex justify-content-center">
-          <img src="~/assets/image/logo.png" alt="" />
-        </div>
-        <h3 class="fw-bold text-white text-center mt-3">Card Babussalam</h3>
-        <p class="text-white text-center mb-4 mx-auto">
-          Login ke akun Card Babussalam untuk mengakses fitur fitur dari
-          aplikasi ini
-        </p>
-        <form @submit.prevent="submit" class="mx-auto">
-          <div class="mb-4">
-            <div class="input-group">
-              <span class="input-group-text" id="basic-addon1">CNC</span>
-              <input
-                name="cnc"
-                type="number"
-                class="form-control"
-                placeholder="12 Digit Angka"
-                required
-              />
+    <div class="container">
+      <div
+        class="login d-block d-md-flex justify-content-center align-items-center"
+      >
+        <div class="login-card">
+          <form @submit.prevent="submit" class="mx-auto">
+            <div class="d-flex justify-content-center mb-5">
+              <img src="~/assets/image/logo.png" alt="" width="80" />
             </div>
-          </div>
-          <div class="mb-4">
-            <div class="input-group mb-3">
-              <input
-                name="pin"
-                :type="type"
-                class="form-control"
-                placeholder="Masukkan PIN"
-                required
-              />
-              <span
-                @click="showpassword"
-                class="input-group-text"
-                id="basic-addon1"
+            <h3 class="fw-bold text-center mt-4">Welcome to Card'M</h3>
+            <p class="text-center mb-5 mx-auto">
+              Kemudahan dalam proses transaksi
+            </p>
+            <div class="mb-3">
+              <div class="input-group">
+                <span class="input-group-text" id="basic-addon1">CNC</span>
+                <input
+                  name="cnc"
+                  type="number"
+                  class="form-control"
+                  placeholder="12 Digit Angka"
+                  required
+                />
+              </div>
+            </div>
+            <div class="mb-lg-3">
+              <div class="input-group mb-3">
+                <input
+                  name="pin"
+                  :type="type"
+                  class="form-control"
+                  placeholder="Masukkan PIN"
+                  required
+                />
+                <span
+                  @click="showpassword"
+                  class="input-group-text"
+                  id="basic-addon1"
+                >
+                  <i v-if="type === 'password'" class="bi bi-eye-slash"></i>
+                  <i v-else class="bi bi-eye"></i>
+                </span>
+              </div>
+            </div>
+            <div class="mb-5">
+              <!-- Route dynamic -->
+              <button v-if="btn" class="btn btn-primary" type="submit">
+                Login to continue
+              </button>
+              <button v-else class="btn btn-secondary" type="button" disabled>
+                <span
+                  class="spinner-border spinner-border-sm"
+                  aria-hidden="true"
+                ></span>
+                <span role="status">Loading...</span>
+              </button>
+            </div>
+            <!-- have account -->
+            <h6 class="text-center mx-auto fw-normal">
+              Belum punya kartu?
+              <nuxt-link to="/signup" class="text-decoration-none"
+                >Aktivasi</nuxt-link
               >
-                <i v-if="type === 'password'" class="bi bi-eye-slash"></i>
-                <i v-else class="bi bi-eye"></i>
-              </span>
-            </div>
-          </div>
-          <div class="mb-3">
-            <!-- Route dynamic -->
-            <button v-if="btn" class="btn btn-primary" type="submit">
-              Masuk
-            </button>
-            <button v-else class="btn btn-secondary" type="button" disabled>
-              <span
-                class="spinner-border spinner-border-sm"
-                aria-hidden="true"
-              ></span>
-              <span role="status">Loading...</span>
-            </button>
-          </div>
-          <!-- have account -->
-          <p class="text-center text-white mx-auto">
-            Belum punya kartu?
-            <nuxt-link to="/signup" class="text-decoration-none text-warning"
-              >Aktivasi</nuxt-link
-            >
-          </p>
-        </form>
+            </h6>
+          </form>
+        </div>
       </div>
     </div>
-    <Footer />
   </div>
 </template>
 
@@ -118,53 +120,5 @@ export default {
 </script>
 
 <style scoped>
-.main {
-  background-image: url(~/assets/image/bg-main.jpg);
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-  height: 100vh;
-}
-input {
-  height: 50px;
-}
-.btn {
-  width: 100%;
-  height: 50px;
-}
-
-/* media */
-form {
-  width: 70%;
-}
-p {
-  width: 70%;
-}
-
-@media screen and (max-width: 362px) {
-  .main {
-    height: max-content;
-  }
-  .container {
-    align-items: start;
-    padding-bottom: 10px;
-  }
-}
-
-@media screen and (max-width: 576px) {
-  .main {
-    height: 100vh;
-  }
-  .container {
-    height: 100%;
-    align-items: center;
-  }
-  /* media */
-  form {
-    width: 100% !important;
-  }
-  p {
-    width: 100% !important;
-  }
-}
+@import url(~/assets/css/index.css);
 </style>
