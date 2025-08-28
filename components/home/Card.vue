42dc55bbd93b744973226e51a7694bcb8260e7cd
haikal <haikal3ahnaf@gmail.com>
2025-08-28 15:47:31 +0700

    blokir kartuu

--- components/home/Card.vue
+++ components/home/Card.vue
@@ -6,54 +6,62 @@
         <img src="~/assets/image/logo.webp" width="30px" height="30px" class="img-card" />
       </div>
       <div class="mb-5">
-        <h2 class="fw-semibold text-white">{{ card?.SK ? card?.SK : '-' }}</h2>
+        <h2 class="fw-semibold text-white">{{ card?.SK ? card?.SK : "-" }}</h2>
       </div>
       <div>
         <!-- <p class="text-white">{{ $store.state.card.nis }}</p> -->
-        <h6 class="text-white text-capitalize">{{ $auth.user.cnc !== '-' ? $auth.user.name : '-' }}</h6>
+        <h6 class="text-white text-capitalize">{{ $auth.user.cnc !== "-" ? $auth.user.name : "-" }}</h6>
       </div>
     </div>
     <div class="fake-card border-none rounded-4 bg-warning"></div>
+
+    <div v-if="card?.Status === `blokir`" class="pt-3">
+      <p class="p-3 bg-danger text-white rounded-4">
+        Kartu anda tak bisa digunakan,
+        <NuxtLink to="/menukartu/blokirkartu">aktifkan</NuxtLink>
+        untuk kembali bertransaksi
+      </p>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-export default {
-  computed: {
-    ...mapState('index', ['card'])
-  },
-};
+  import { mapState } from "vuex";
+  export default {
+    computed: {
+      ...mapState("index", ["card"]),
+    },
+  };
 </script>
 
 <style scoped>
-.greyscale {
-  filter: grayscale(100%);
-}
-.card {
-  background-image: url(~/assets/image/card.png);
-  background-position: center;
-  background-size: cover;
-  background-repeat: no-repeat;
-}
+  .greyscale {
+    filter: grayscale(100%);
+  }
+  .card {
+    background-image: url(~/assets/image/card.png);
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
 
-.fake-card {
-  margin: -39px auto 0;
-  height: 50px;
-  width: 90%;
-  z-index: -2;
-}
+  .fake-card {
+    margin: -39px auto 0;
+    height: 50px;
+    width: 90%;
+    z-index: -2;
+  }
 
-h2 {
-  font-size: 24px;
-}
+  h2 {
+    font-size: 24px;
+  }
 
-p {
-  font-size: 12px;
-}
+  p {
+    font-size: 12px;
+  }
 
-h6 {
-  margin-top: -7px;
-  font-size: 14px;
-}
+  h6 {
+    margin-top: -7px;
+    font-size: 14px;
+  }
 </style>
--- pages/menukartu/blokirkartu.vue
+++ pages/menukartu/blokirkartu.vue
@@ -20,19 +20,15 @@
             </h4>
           </div>
           <div>
-            <input v-model="blok" class="form-check-input mb-4" type="checkbox" role="switch"
-              id="flexSwitchCheckChecked" checked />
+            <input v-model="blok" class="form-check-input mb-4" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked />
           </div>
         </div>
         <div class="card bg-gradient shadow">
           <p v-if="blok" class="p-3 bg-warning">
-            Perhatian! Tindakan menonaktifkan kartu akan menyebabkan kartu tidak
-            bisa digunakan untuk bertransaksi di outlet. Hanya nonaktifkan
-            ketika kartu anda hilang
+            Perhatian! Tindakan menonaktifkan kartu akan menyebabkan kartu tidak bisa digunakan untuk bertransaksi di outlet. Hanya nonaktifkan ketika
+            kartu anda hilang
           </p>
-          <p v-else class="p-3 bg-primary text-white">
-            Kartu anda tak bisa digunakan, aktifkan untuk kembali bertransaksi
-          </p>
+          <p v-else class="p-3 bg-danger text-white">Kartu anda tak bisa digunakan, aktifkan untuk kembali bertransaksi</p>
         </div>
       </div>
     </div>
@@ -40,49 +36,49 @@
 </template>
 
 <script>
-import Swal from "sweetalert2";
-import { mapGetters, mapState } from "vuex";
+  import Swal from "sweetalert2";
+  import { mapGetters, mapState } from "vuex";
 
-export default {
-  async asyncData({ store }) {
-    store.dispatch('blokir/renderPage')
-  },
-  computed: {
-    ...mapGetters('blokir', ['getStatus']),
-    blok: {
-      get() {
-        const condition = this.getStatus === "active" ? true : false;
-        return condition;
-      },
-      set(value) {
-        const condition = value ? "active" : "blokir";
-        this.$store.dispatch('blokir/setStatus', condition);
+  export default {
+    async asyncData({ store }) {
+      store.dispatch("blokir/renderPage");
+    },
+    computed: {
+      ...mapGetters("blokir", ["getStatus"]),
+      blok: {
+        get() {
+          const condition = this.getStatus === "active" ? true : false;
+          return condition;
+        },
+        set(value) {
+          const condition = value ? "active" : "blokir";
+          this.$store.dispatch("blokir/setStatus", condition);
+        },
       },
     },
-  },
-};
+  };
 </script>
 
 <style scoped>
-.fake-image {
-  opacity: 0;
-}
+  .fake-image {
+    opacity: 0;
+  }
 
-input {
-  height: 25px;
-  width: 50px !important;
-}
+  input {
+    height: 25px;
+    width: 50px !important;
+  }
 
-h4 {
-  margin-left: -40px;
-}
+  h4 {
+    margin-left: -40px;
+  }
 
-.block {
-  color: grey;
-}
+  .block {
+    color: grey;
+  }
 
-p {
-  font-size: 16px;
-  margin-bottom: 0 !important;
-}
+  p {
+    font-size: 16px;
+    margin-bottom: 0 !important;
+  }
 </style>
