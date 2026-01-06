c8b76c187d6efa62f5fbb4f35d74baaee447ce64
haikal <haikal3ahnaf@gmail.com>
2026-01-06 15:56:49 +0700

    ziyadah & change pw

--- components/mutabaah/MutabaahTable.vue
+++ components/mutabaah/MutabaahTable.vue
@@ -2,7 +2,10 @@
   <div>
     <div class="container">
       <div class="total-wrapper mb-4 d-flex flex-column justify-content-center align-items-center">
-        <p class="py-1 px-2 bg-secondary-subtle rounded-3"> <i class="bi bi-book me-1"></i> Total Hafalan</p>
+        <p class="py-1 px-2 bg-secondary-subtle rounded-3">
+          <i class="bi bi-book me-1"></i>
+          Total Hafalan
+        </p>
         <h1 class="fw-bold">{{ datas.Juz }} Juz</h1>
         <h6>{{ datas.totalPages }} halaman</h6>
       </div>
@@ -16,17 +19,23 @@
         <div class="line-bottom bg-success rounded-5"></div>
       </div> -->
         <div class="card p-3 bg-light border-0 shadow-sm rounded-4">
-          <p class="mb-2"><i class="bi bi-book me-2"></i>Hafalan Awal</p>
+          <p class="mb-2">
+            <i class="bi bi-book me-2"></i>
+            Hafalan Awal
+          </p>
           <h1 class="mb-1" style="font-family: 'Noto Kufi Arabic', sans-serif; font-weight: 600">{{ datas?.oldestFrom?.From?.name }}</h1>
           <h6 class="text-secondary mb-3">Ayat {{ datas?.oldestFrom?.From?.ayat?.number }}</h6>
-          <p class="mb-0">{{ changeDate(datas?.oldestFrom?.SK?.split(' ')[0]) }}</p>
+          <p class="mb-0">{{ changeDate(datas?.oldestFrom?.SK?.split(" ")[0]) }}</p>
           <!-- <div class="line-bottom bg-success rounded-5"></div> -->
         </div>
         <div class="card p-3 bg-light border-0 shadow-sm rounded-4">
-          <p class="mb-2"><i class="bi bi-book me-2"></i>Hafalan Terakhir</p>
+          <p class="mb-2">
+            <i class="bi bi-book me-2"></i>
+            Hafalan Terakhir
+          </p>
           <h1 class="mb-1" style="font-family: 'Noto Kufi Arabic', sans-serif; font-weight: 600">{{ datas?.earliestTo?.To?.name }}</h1>
           <h6 class="text-secondary mb-3">Ayat {{ datas?.earliestTo?.To?.ayat?.number }}</h6>
-          <p class="mb-0">{{ changeDate(datas?.earliestTo?.SK?.split(' ')[0]) }}</p>
+          <p class="mb-0">{{ changeDate(datas?.earliestTo?.SK?.split(" ")[0]) }}</p>
           <!-- <div class="line-bottom bg-success rounded-5"></div> -->
         </div>
         <!-- <div class="card p-3 bg-danger-subtle border-0 shadow-sm rounded-4">
@@ -38,22 +47,28 @@
       </div>
 
       <!-- history -->
-      <div class="history" style="padding-bottom: 70px !important;">
-        <p class="mb-3" style="font-size: 10px;">Riwayat Ziyadah</p>
+      <div class="history" style="padding-bottom: 70px !important">
+        <p class="mb-3" style="font-size: 10px">Riwayat Ziyadah</p>
         <div v-for="(data, index) in datas?.hafalan" :key="index" class="card mb-2 bg-light p-2 border-0 rounded-4">
           <div class="list-history p-1 d-flex justify-content-between">
-            <div class="info d-flex gap-2">
-              <div class="icon py-1 px-2 bg-primary-subtle rounded-circle">
-                <i class="bi bi-info"></i>
+            <div class="info d-flex gap-2 align-items-center">
+              <div
+                class="icon d-flex align-items-center justify-content-center bg-primary-subtle rounded-circle"
+                style="width: 45px; height: 45px; min-width: 45px">
+                <i class="bi bi-info" style="font-size: 1.2rem"></i>
               </div>
+
               <div class="d-flex flex-column">
-                <p class="mb-0">Ayat {{ data.From.ayat.number }} - {{ data.To.ayat.number }}</p>
+                <p class="mb-0">{{ data.SK.split(" ")[0] }}</p>
+                <p class="mb-0">{{ data.SK.slice(11, 16) }}</p>
                 <p class="mb-0">{{ data.Page }} Halaman</p>
               </div>
             </div>
+
             <div class="from d-flex gap-1 align-items-center">
-              <div class="d-flex align-items-center">
+              <div class="d-flex flex-column">
                 <h1 class="mb-0">{{ data.From.name }} - {{ data.To.name }}</h1>
+                <p class="mb-0">Ayat {{ data.From.ayat.number }} - {{ data.To.ayat.number }}</p>
               </div>
             </div>
           </div>
@@ -172,34 +187,33 @@
 </template>
 
 <script>
-import moment from 'moment';
-import 'moment/locale/id';
-import { mapState } from 'vuex'
-export default {
-  computed: {
-    ...mapState('mutabaah', ['datas'])
-  },
-  methods: {
-    formatDate(dateString) {
-      // Set the locale and format the date
-      moment.locale('id');
-      return moment(dateString).format('DD MMMM YYYY');
+  import moment from "moment";
+  import "moment/locale/id";
+  import { mapState } from "vuex";
+  export default {
+    computed: {
+      ...mapState("mutabaah", ["datas"]),
     },
-    changeDate(dateString) {
-      if ( dateString ) {
-        const date = new Date(dateString);
+    methods: {
+      formatDate(dateString) {
+        // Set the locale and format the date
+        moment.locale("id");
+        return moment(dateString).format("DD MMMM YYYY");
+      },
+      changeDate(dateString) {
+        if (dateString) {
+          const date = new Date(dateString);
 
-        const options = { day: 'numeric', month: 'long', year: 'numeric' };
-        const formattedDate = new Intl.DateTimeFormat('id-ID', options).format(date);
+          const options = { day: "numeric", month: "long", year: "numeric" };
+          const formattedDate = new Intl.DateTimeFormat("id-ID", options).format(date);
 
-        return formattedDate
-      } else {
-        return '-'
-      }
-    }
-  },
-}
+          return formattedDate;
+        } else {
+          return "-";
+        }
+      },
+    },
+  };
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
--- pages/menukartu/gantipassword.vue
+++ pages/menukartu/gantipassword.vue
@@ -4,11 +4,8 @@
       <div class="all d-flex flex-column align-items-center justify-content-center">
         <img src="~/assets/image/success.png" alt="" width="200" />
         <h1 class="text-primary fw-bold mt-5 text-center">Perubahan Berhasil!</h1>
-        <p class="w-75 text-center mb-5">
-          Kembali ke Card untuk mengakses fitur kembali
-        </p>
-        <nuxt-link to="/card" class="btn btn-primary text-decoration-none py-2 px-5 mt-3">Kembali ke
-          Card</nuxt-link>
+        <p class="w-75 text-center mb-5">Kembali ke Card untuk mengakses fitur kembali</p>
+        <nuxt-link to="/card" class="btn btn-primary text-decoration-none py-2 px-5 mt-3">Kembali ke Card</nuxt-link>
       </div>
     </div>
     <div v-else class="media mx-auto">
@@ -26,14 +23,16 @@
         <div class="mt-5 mb-4">
           <h1 class="fw-semibold">Buat Password Baru</h1>
           <p class="fw-light text-secondary">
-            untuk mengamankan akunmu, ganti <br />
+            untuk mengamankan akunmu, ganti
+            <br />
             dengan password yang lebih kuat
           </p>
         </div>
 
         <form @submit.prevent="submit">
           <!-- Pin Lama -->
-          <label for="pin" class="mb-2">Masukkan Password Lama</label><br />
+          <label for="pin" class="mb-2">Masukkan Password Lama</label>
+          <br />
           <div class="input-group mb-3">
             <input id="pin" name="oldPassword" :type="type" class="form-control" placeholder="Password Lama" required />
             <span @click="showpassword" class="input-group-text" id="basic-addon1">
@@ -42,9 +41,17 @@
             </span>
           </div>
           <!-- Pin Baru -->
-          <label for="pinCheck" class="mb-2">Masukkan Password Baru</label><br />
+          <label for="pinCheck" class="mb-2">Masukkan Password Baru</label>
+          <br />
           <div class="input-group mb-3">
-            <input id="newPin" name="newPassword" :type="type" class="form-control" placeholder="Password Baru"
+            <input
+              id="newPin"
+              name="newPassword"
+              minlength="6"
+              maxlength="24"
+              :type="type"
+              class="form-control"
+              placeholder="Password Baru"
               required />
             <span @click="showpassword" class="input-group-text" id="basic-addon1">
               <i v-if="type === 'password'" class="bi bi-eye-slash"></i>
@@ -52,9 +59,17 @@
             </span>
           </div>
           <!-- konfirmasi Pin -->
-          <label for="pinCheck" class="mb-2">Konfirmasi Password</label><br />
+          <label for="pinCheck" class="mb-2">Konfirmasi Password</label>
+          <br />
           <div class="input-group mb-4">
-            <input id="pinCheck" name="passwordCheck" :type="type" class="form-control" placeholder="Konfirmasi"
+            <input
+              id="pinCheck"
+              name="passwordCheck"
+              minlength="6"
+              maxlength="24"
+              :type="type"
+              class="form-control"
+              placeholder="Konfirmasi"
               required />
             <span @click="showpassword" class="input-group-text" id="basic-addon1">
               <i v-if="type === 'password'" class="bi bi-eye-slash"></i>
@@ -62,9 +77,7 @@
             </span>
           </div>
           <!-- Submit -->
-          <button v-if="btn" type="submit" class="btn btn-primary">
-            Simpan Password
-          </button>
+          <button v-if="btn" type="submit" class="btn btn-primary">Simpan Password</button>
           <button v-else class="btn btn-secondary" type="button" disabled>
             <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
             <span role="status">Loading...</span>
@@ -76,103 +89,103 @@
 </template>
 
 <script>
-import Swal from "sweetalert2";
+  import Swal from "sweetalert2";
 
-export default {
-  data() {
-    return {
-      type: "password",
-      btn: true,
-      confirmation: false
-    };
-  },
-  methods: {
-    async submit(event) {
-      //manggil API
-      this.btn = false;
-      const data = Object.fromEntries(new FormData(event.target));
-      if (data.newPassword !== data.passwordCheck) {
-        this.btn = true;
-        return Swal.fire({
-          icon: "warning",
-          text: "Konfirmasi Password salah",
-          showConfirmButton: false,
-          timer: 1500,
-        });
-      }
-      data['accessToken'] = this.$cookies.get('AccessToken')
-      delete data.passwordCheck
-      try {
-        const result = await this.$axios.$put(`change-password`, data);
-        if (result.message = 'success') {
+  export default {
+    data() {
+      return {
+        type: "password",
+        btn: true,
+        confirmation: false,
+      };
+    },
+    methods: {
+      async submit(event) {
+        //manggil API
+        this.btn = false;
+        const data = Object.fromEntries(new FormData(event.target));
+        if (data.newPassword !== data.passwordCheck) {
           this.btn = true;
-          this.confirmation = true
+          return Swal.fire({
+            icon: "warning",
+            text: "Konfirmasi Password salah",
+            showConfirmButton: false,
+            timer: 1500,
+          });
         }
-      } catch (error) {
-        this.btn = true;
-        Swal.fire({
-          icon: "warning",
-          text: 'password sebelumnya salah atau gagal mengubah',
-          showConfirmButton: false,
-          timer: 1500,
-        });
-      }
+        data["accessToken"] = this.$cookies.get("AccessToken");
+        delete data.passwordCheck;
+        try {
+          const result = await this.$axios.$put(`change-password`, data);
+          if ((result.message = "success")) {
+            this.btn = true;
+            this.confirmation = true;
+          }
+        } catch (error) {
+          this.btn = true;
+          Swal.fire({
+            icon: "warning",
+            text: error.response.data.message,
+            showConfirmButton: false,
+            timer: 2500,
+          });
+        }
+      },
+      showpassword() {
+        const show = this.type === "password" ? "text" : "password";
+        this.type = show;
+      },
     },
-    showpassword() {
-      const show = this.type === "password" ? "text" : "password";
-      this.type = show;
-    },
-  },
-};
+  };
 </script>
 
 <style scoped>
-@media screen and (max-width: 576px) {
-  .container {
-    height: 100vh;
+  @media screen and (max-width: 576px) {
+    .container {
+      height: 100vh;
+    }
+
+    .all {
+      height: 100%;
+    }
+  }
+  .fake-image {
+    opacity: 0;
   }
 
-  .all {
-    height: 100%;
+  /* judul */
+  h1 {
+    font-size: 24px;
   }
-}
-.fake-image {
-  opacity: 0;
-}
 
-/* judul */
-h1 {
-  font-size: 24px;
-}
+  p {
+    font-size: 14px;
+  }
 
-p {
-  font-size: 14px;
-}
+  /* form */
+  input {
+    height: 50px;
+  }
 
-/* form */
-input {
-  height: 50px;
-}
+  button {
+    width: 100%;
+    height: 50px;
+  }
 
-button {
-  width: 100%;
-  height: 50px;
-}
-
-.media {
-  width: 40%;
-}
-
-@media screen and (max-width: 576px) {
   .media {
-    width: 100% !important;
+    width: 40%;
   }
-}
 
-/* tablet */
-@media screen and (max-width: 991px) {
-  .media {
-    width: 70%;
+  @media screen and (max-width: 576px) {
+    .media {
+      width: 100% !important;
+    }
   }
-}
+
+  /* tablet */
+  @media screen and (max-width: 991px) {
+    .media {
+      width: 70%;
+    }
+  }
 </style>
